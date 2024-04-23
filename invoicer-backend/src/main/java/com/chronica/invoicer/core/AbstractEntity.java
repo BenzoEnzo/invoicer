497197842946c1c782b33e6d0cd2f367d9e0a52f
GucioWons <wgutek1@wp.pl>
2024-04-23 20:21:00 +0200

    Abstract-entity

---
+++ invoicer-backend/src/main/java/com/chronica/invoicer/core/AbstractEntity.java
@@ -0,0 +1,17 @@
+package com.chronica.invoicer.core;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class AbstractEntity <T extends BaseEntity> {
+    private T realEntity;
+    private Date createdAt;
+}

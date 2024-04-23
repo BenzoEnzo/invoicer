60c7da17560e0c4901cd76fb7adc25a3e72150fb
GucioWons <wgutek1@wp.pl>
2024-04-23 19:25:40 +0200

    Invoicer-backend-initial-commit

---
+++ invoicer-backend/src/test/java/com/chronica/invoicer/InvoicerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronica.invoicer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

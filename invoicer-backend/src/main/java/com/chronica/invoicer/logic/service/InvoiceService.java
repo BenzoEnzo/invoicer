fbbe3fc4d98c674f3ca38fd38635178c8551d272
Kuba <goxer@o2.pl>
2024-05-28 17:03:38 +0200

    calculate functions

--- invoicer-backend/src/main/java/com/chronica/invoicer/logic/service/InvoiceService.java
+++ invoicer-backend/src/main/java/com/chronica/invoicer/logic/service/InvoiceService.java
@@ -17,6 +17,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,22 +32,45 @@ public class InvoiceService {
     private final InvoicePriceMapper invoicePriceMapper;
     private final InvoiceItemMapper invoiceItemMapper;
 
+    @Transactional
     public InvoiceDTO create(InvoiceDTO invoiceDTO){
         InvoicePrice invoicePrice = invoicePriceMapper.mapToEntity(invoiceDTO.getInvoicePrice());
+
         List<InvoiceItem> invoiceItems = invoiceDTO
                 .getInvoicePrice()
                 .getInvoiceItems()
                 .stream().map(invoiceItemMapper::mapToEntity).toList();
-        invoicePrice.setInvoiceItems(invoiceItems);
-
-        invoiceItemRepository.saveAll(invoiceItems);
-        invoicePriceRepository.save(invoicePrice);
 
         Invoice invoice = invoiceMapper.mapToEntity(invoiceDTO);
         invoice.setInvoicePrice(invoicePrice);
 
+        invoicePrice.setInvoiceItems(invoiceItems);
+        invoicePrice.setInvoice(invoice);
+
+        calculatePartialCost(invoicePrice);
+        calculatePriceNetto(invoicePrice);
+
         invoiceRepository.save(invoice);
 
         return invoiceMapper.mapToDTO(invoice);
+    }
+
+    private void calculatePartialCost(InvoicePrice invoicePrice){
+        List<InvoiceItem> invoiceItems = invoicePrice.getInvoiceItems();
+
+        invoiceItems.forEach(g -> g.setPartialPrice(g.getQuantity()
+                .multiply(g.getProduct().getNetPrice())));
+
+        invoiceItemRepository.saveAll(invoiceItems);
+    }
+
+    private void calculatePriceNetto(InvoicePrice invoicePrice){
+        invoicePrice.setNetAmount(invoicePrice
+                .getInvoiceItems()
+                .stream()
+                .map(InvoiceItem::getPartialPrice)
+                .reduce(BigDecimal.ZERO,BigDecimal::add));
+
+        invoicePriceRepository.save(invoicePrice);
     }
 }

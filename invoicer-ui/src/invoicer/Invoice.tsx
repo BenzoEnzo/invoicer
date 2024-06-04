30886dd9dd05998a5bb579f4819b41fc2dbf26b7
Kuba <goxer@o2.pl>
2024-06-04 21:38:42 +0200

    improvements

--- invoicer-ui/src/invoicer/Invoice.tsx
+++ invoicer-ui/src/invoicer/Invoice.tsx
@@ -1,9 +1,73 @@
 import React, {useEffect, useState} from 'react';
+import '../shared/style/form.css';
+import '../shared/style/container.css';
+import {CompanyDTO} from "../company/model/CompanyDTO";
+import CompanyAPI from '../company/service/CompanyAPI';
 function Invoice(){
+    const [customerId, setCustomerId] = useState('');
+    const [error, setError] = useState<string | null>(null);
+    const [customer, setCustomer] = useState<CompanyDTO>({
+        name: '',
+        shortName: '',
+        street: '',
+        building: 0,
+        apartment: 0,
+        zipCode: '',
+        city: '',
+        nip: 0,
+    });
+
+    const handleCustomerIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCustomerId(event.target.value);
+    };
+
+    const handleCustomerSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        try {
+            const data = await CompanyAPI.getCompanyById(parseInt(customerId, 10));
+            setCustomer(data);
+        } catch (error) {
+            setError('Nie znaleziono takiego uzytkownika.');
+        }
+    };
 
     return (
         <>
-        <div><h2>Tworzenie faktury</h2></div>
+
+        <div>
+            <h2>Tworzenie faktury</h2>
+        </div>
+
+        <div className="main-container">
+            <div className="additional-container">
+                <form onSubmit={handleCustomerSubmit}>
+                    <div className="input-form">
+                        <h3>Dla kogo:</h3>
+                        <input
+                            type="number"
+                            id="customerId"
+                            value={customerId}
+                            onChange={handleCustomerIdChange}
+                            required
+                        />
+                        <button type="submit">OK</button>
+                    </div>
+                </form>
+            </div>
+
+            <div className="additional-container">
+                <div className="content-container">
+                    <div className="head-container">
+                        <center><h2>{customer.name}</h2></center>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="main-container">
+
+        </div>
         </>
     );
 }

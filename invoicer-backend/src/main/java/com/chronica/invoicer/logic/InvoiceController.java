c1455a81effc9df37f8af38c4839ca03631a4356
Jakub Bartuś <141475592+BenzoEnzo@users.noreply.github.com>
2024-06-05 19:13:06 +0200

    Backend/get invoices list (#16)
    * ui invoice tab init
    
    * improvements
    
    * improvements for invoice generator
    
    * list of seller invoices implemented

--- invoicer-backend/src/main/java/com/chronica/invoicer/logic/InvoiceController.java
+++ invoicer-backend/src/main/java/com/chronica/invoicer/logic/InvoiceController.java
@@ -1,19 +1,13 @@
 package com.chronica.invoicer.logic;
 
 import com.chronica.invoicer.invoice.dto.InvoiceDTO;
-import com.chronica.invoicer.invoice.entity.Invoice;
 import com.chronica.invoicer.logic.service.InvoiceService;
 import com.chronica.invoicer.logic.service.JasperService;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import net.sf.jasperreports.engine.JRException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -23,6 +17,11 @@ public class InvoiceController {
 
     private final InvoiceService invoiceService;
     private final JasperService jasperService;
+
+    @GetMapping("/seller/{sellerId}")
+    public ResponseEntity<List<InvoiceDTO>> getSellerInvoices(@PathVariable Long sellerId){
+        return new ResponseEntity<>(invoiceService.getSellerInvoices(sellerId), HttpStatus.OK);
+    }
 
     @PostMapping
     public ResponseEntity<InvoiceDTO> createInvoice(@RequestBody InvoiceDTO invoiceDTO) {

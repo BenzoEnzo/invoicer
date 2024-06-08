51ebe40b1f9d2f53b248953a8b7388f5fa61e7ea
Jakub Bartuś <141475592+BenzoEnzo@users.noreply.github.com>
2024-06-08 20:55:28 +0200

    Frontend/invoices list view (#18)
    * ui invoice tab init
    
    * improvements
    
    * improvements for invoice generator
    
    * list of seller invoices implemented
    
    * list of seller invoices implemented
    
    * comm
    
    * savv
    
    * Revert "comm"
    
    This reverts commit 5d04dd0a
    
    * invoices list

--- invoicer-ui/src/invoicer/Invoice.tsx
+++ invoicer-ui/src/invoicer/Invoice.tsx
@@ -1,11 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../shared/style/form.css';
 import '../shared/style/container.css';
 import '../shared/style/table.css';
-import {CompanyDTO} from "../company/model/CompanyDTO";
+import { CompanyDTO } from "../company/model/CompanyDTO";
 import CompanyAPI from '../company/service/CompanyAPI';
+import InvoiceAPI from '../invoicer/service/InvoiceAPI';
 import { useSelectedProducts } from './service/SelectProductState';
+import { InvoiceDTO } from './model/InvoiceDTO';
+import { format } from 'date-fns';
+import ProductAPI from "../product/service/ProductAPI";
+
 function Invoice() {
+    const formatDate = (date:any) => {
+        return date ? format(new Date(date), 'yyyy-MM-dd') : 'N/A';
+    };
     const [customerId, setCustomerId] = useState('');
     const [error, setError] = useState<string | null>(null);
     const [customer, setCustomer] = useState<CompanyDTO>({
@@ -18,7 +26,7 @@ function Invoice() {
         city: '',
         nip: 0,
     });
-
+    const [invoices, setInvoices] = useState<InvoiceDTO[]>([]);
     const { selectedProducts } = useSelectedProducts();
 
     const handleCustomerIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,14 +35,52 @@ function Invoice() {
 
     const handleCustomerSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-
         try {
             const data = await CompanyAPI.getCompanyById(parseInt(customerId, 10));
             setCustomer(data);
         } catch (error) {
-            setError('Nie znaleziono takiego uzytkownika.');
+            setError('Nie znaleziono takiego użytkownika.');
         }
     };
+
+    const handleCreateInvoice = async () => {
+        try {
+            const createdProduct = await InvoiceAPI.createInvoice({
+
+            });
+        } catch (error) {
+            console.error('Error creating invoice:', error);
+        }
+    };
+
+    const generateInvoice = async (invoiceId: number | undefined) => {
+        setError(null);
+        try {
+            const blob = await InvoiceAPI.generateInvoice(invoiceId);
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `invoice_${invoiceId}.pdf`;
+            a.click();
+            window.URL.revokeObjectURL(url);
+        } catch (error) {
+            setError('Błąd generowania faktury');
+        }
+    };
+
+    useEffect(() => {
+        const fetchInvoices = async () => {
+            setError(null);
+            try {
+                const data = await InvoiceAPI.getSellerInvoices(1);
+                setInvoices(data);
+            } catch (error) {
+                setError('Nie udało się pobrać faktur.');
+            }
+        };
+
+        fetchInvoices();
+    }, [customerId]);
 
     return (
         <>
@@ -43,24 +89,58 @@ function Invoice() {
             </div>
 
             <div className="main-container">
+
                 <div className="additional-container">
-                    <form onSubmit={handleCustomerSubmit}>
-                        <div className="input-form">
-                            <h3>Dla kogo:</h3>
-                            <input
-                                type="number"
-                                id="customerId"
-                                value={customerId}
-                                onChange={handleCustomerIdChange}
-                                required
-                            />
-                            <button type="submit">OK</button>
-                        </div>
-                    </form>
+                    <div className="little-container">
+                        <form onSubmit={handleCustomerSubmit}>
+                            <div className="input-form">
+                                <h3>Dla kogo:</h3>
+                                <input
+                                    type="number"
+                                    id="customerId"
+                                    value={customerId}
+                                    onChange={handleCustomerIdChange}
+                                    required
+                                />
+                                <button type="submit">OK</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="little-container">
+                        <h3>Lista wystawionych faktur:</h3>
+                        <table className="table">
+                            <thead>
+                            <tr>
+                                <th>Symbol</th>
+                                <th>Sprzedawca</th>
+                                <th>Klient</th>
+                                <th>Kwota netto</th>
+                                <th>Kwota VAT</th>
+                                <th>Kwota brutto</th>
+                                <th>Widok</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {invoices.map((invoice) => (
+                                <tr key={invoice.id}>
+                                    <td>{invoice.symbol}</td>
+                                    <td>{invoice.seller?.name}</td>
+                                    <td>{invoice.customer?.name}</td>
+                                    <td>{invoice.invoicePrice?.netAmount?.toFixed(2)}</td>
+                                    <td>{invoice.invoicePrice?.vatAmount?.toFixed(2)}</td>
+                                    <td>{invoice.invoicePrice?.brutAmount?.toFixed(2)}</td>
+                                    <td>
+                                        <button onClick={() => generateInvoice(invoice.id)}>Pobierz</button>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
                 <div className="additional-container">
-                    <div className="content-container">
+                <div className="content-container">
                         <div className="head-container">
                             <center><h2>{customer.name}</h2></center>
                         </div>
@@ -75,7 +155,7 @@ function Invoice() {
                                     <th>Cena netto</th>
                                     <th>Jednostka</th>
                                     <th>Stawka VAT</th>
-                                    <th>Ilosc</th>
+                                    <th>Ilość</th>
                                 </tr>
                                 </thead>
                                 <tbody>
@@ -94,7 +174,7 @@ function Invoice() {
                         </div>
 
                         <div className="bottom-container">
-                            <button type="submit">Generuj fakture</button>
+                            <button type="submit">Generuj fakturę</button>
                         </div>
 
                     </div>

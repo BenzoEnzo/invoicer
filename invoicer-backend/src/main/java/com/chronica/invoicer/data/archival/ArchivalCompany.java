5a886cf2565285156174f3f204e29342f84c4d1d
GucioWons <wgutek1@wp.pl>
2024-06-09 22:35:45 +0200

    Archival-corrections

--- invoicer-backend/src/main/java/com/chronica/invoicer/data/archival/ArchivalCompany.java
+++ invoicer-backend/src/main/java/com/chronica/invoicer/data/archival/ArchivalCompany.java
@@ -2,6 +2,7 @@ package com.chronica.invoicer.data.archival;
 
 import com.chronica.invoicer.data.entity.Company;
 import com.chronica.invoicer.data.ArchivalEntity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,10 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class ArchivalCompany extends ArchivalEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String shortName;
@@ -21,5 +25,7 @@ public class ArchivalCompany extends ArchivalEntity {
     private String zipCode;
     private String city;
     private Integer nip;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "realCompany_id")
     private Company realCompany;
 }
